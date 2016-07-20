13c62f470a5f11e8ca606aa729ed2cadc8b23cf8
Alexander Evsyuchenya <alexander.evsyuchenya@adms-imac-2.hiqo-solutions.com>
2016-07-20 16:14:52 +0300

    added files

---
+++ MPCF_Multistream_test/MPCF_Multistream_test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MPCF_Multistream_test
+//
+//  Created by Alexander Evsyuchenya on 7/20/16.
+//  Copyright © 2016 baydet. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MPCF_Multistream_test/MPCF_Multistream_test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MPCF_Multistream_test
+//
+//  Created by Alexander Evsyuchenya on 7/20/16.
+//  Copyright © 2016 baydet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
